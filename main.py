3f7d2e2665b12da63ee105e031a2fbde58302e2f
K1A2 <jckim0307@gmail.com>
2024-02-12 21:19:52 +0900

    feat: user model 생성

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from api.api_v1.api import api_router
 from core.config import settings
+import models
+from database.session import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
--- models.py
+++ models.py
@@ -2,10 +2,10 @@ from sqlalchemy import Column, String, Integer, String, DateTime, Boolean
 from datetime import datetime
 from database.session import Base
 
-class UserBase(Base):
+class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String, unique=True, nullable=False, index=True)
-    hashed_password = Column(String, nullable=False)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    hashed_password = Column(String(255), nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
