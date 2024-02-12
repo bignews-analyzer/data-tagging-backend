40a29ab7f58809a72f33d836de6ea76712f25f70
K1A2 <jckim0307@gmail.com>
2024-02-12 23:14:53 +0900

    feat: token generate 추가

--- core/config.py
+++ core/config.py
@@ -7,7 +7,16 @@ load_dotenv()
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    MYSQL_URL = os.environ.get('MYSQL_URL')
+
+    MYSQL_URL: str = os.environ.get('MYSQL_URL')
+
+    ACCESS_TOKEN_ENCODE_ALGORITHM: str = os.environ.get('ACCESS_TOKEN_ENCODE_ALGORITHM')
+    ACCESS_SECRET_KEY: str = os.environ.get('ACCESS_SECRET_KEY')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 2
+
+    REFRESH_TOKEN_ENCODE_ALGORITHM: str = os.environ.get('REFRESH_TOKEN_ENCODE_ALGORITHM')
+    REFRESH_SECRET_KEY: str = os.environ.get('REFRESH_SECRET_KEY')
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 14
 
     class Config:
         case_sensitive = True
