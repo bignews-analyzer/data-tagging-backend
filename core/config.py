0b4393ab57f75878ba19db5c11f6e568ace2ebc7
K1A2 <jckim0307@gmail.com>
2024-02-12 19:44:57 +0900

    feat: 데이터베이스 연동

--- core/config.py
+++ core/config.py
@@ -1,8 +1,13 @@
 from pydantic import BaseSettings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
+    MYSQL_URL = os.environ.get('MYSQL_URL')
 
     class Config:
         case_sensitive = True
